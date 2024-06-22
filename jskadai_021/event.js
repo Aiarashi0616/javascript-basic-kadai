64d474f4849289d2a005d2f736af7eb105330b34
Aiarashi0616 <160582175+Aiarashi0616@users.noreply.github.com>
2024-06-22 17:25:13 +0900

    Add files via upload

---
+++ jskadai_021/event.js
@@ -0,0 +1,9 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('abc-btn');
+//textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+ 
+// 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+setTimeout(() => {
+  text.textContent = 'ボタンをクリックしました。';
+  }, 2000);
